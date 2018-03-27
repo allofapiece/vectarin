10160346a734b6cf47b4772bed9ccfd084c29899
allofapiece <stasana1998@tut.by>
2018-03-27 23:17:07 +0300

    stas-1.0 Create project, set up necessary

---
+++ src/Controller/QuizController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Listratsenka Stas
+ * Date: 27.03.2018
+ * Time: 20:36
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class QuizController extends Controller
+{
+
+}
---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Listratsenka Stas
+ * Date: 27.03.2018
+ * Time: 20:34
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+
+}
