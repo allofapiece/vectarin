a6a84677b66a0c29e243749d85415e6e25af0d0e
allofapiece <stasana1998@tut.by>
2018-03-30 17:34:53 +0300

    Add bootstrap, jquery, sass-loader, etc.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,10 +8,30 @@
 
 namespace App\Controller;
 
-
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SecurityController extends Controller
 {
+    /**
+     * @Route("/sign-in", name="signIn")
+     */
+    public function signInAction(){
+        return $this->render('security/signin.html.twig');
+    }
 
+    /**
+     * @Route("/log-in", name="logIn")
+     */
+    public function logInAction(){
+        return new Response();
+    }
+
+    /**
+     * @Route("/recovery", name="recovery")
+     */
+    public function passwordRecoveryAction(){
+
+    }
 }
