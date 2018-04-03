baf3642a0a894944c2860cf5e7b0c6c5eabe37f0
allofapiece <stasana1998@tut.by>
2018-04-03 20:36:50 +0300

    Modified question controller by rendering form to template

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,24 +1,67 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Listratsenka Stas
- * Date: 27.03.2018
- * Time: 20:36
- */
 
 namespace App\Controller;
 
 
+use App\Entity\Question;
+use App\Form\Question\QuestionCreateType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class QuestionController extends Controller
 {
     /**
-     * @Route("/questions", name="questions")
+     * @Route("/admin/question/create", name="question.create")
+     * @return Response
      */
-    public function quizzesAction(){
+    public function createQuestion(Request $request)
+    {
+        $question = new Question();
+
+        $form = $this->createForm(QuestionCreateType::class, $question);
+
+        $form->handleRequest($request);
+
+        return $this->render('questions/create_question.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/question/update/{id}",name="question.create.id")
+     * @return Response
+     */
+    public function updateQuestion()
+    {
+        return new Response();
+    }
+
+    /**
+     * @Route("/admin/question/delete/{id}",name="question.delete.id")
+     * @return Response
+     */
+    public function deleteQuestion()
+    {
+        return new Response();
+    }
+
+    /**
+     * @Route("/admin/question/show",name="question.show")
+     * @return Response
+     */
+    public function showQuestions()
+    {
         return $this->render('questions/questions.html.twig');
+    }
+
+    /**
+     * @Route("/admin/question/show/{id}",name="question.show.id")
+     * @return Response
+     */
+    public function showQuestionById()
+    {
+        return new Response();
     }
 }
