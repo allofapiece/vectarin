47a8813a1877d0234985adb556b246f6287ff0aa
Alexandr Graschenko <allseeing98@gmail.com>
2018-04-04 17:18:10 +0300

    add recovery token

--- src/Form/RecoveryPassword/RecoveryTypeEmail.php
+++ src/Form/RecoveryPassword/RecoveryTypeEmail.php
@@ -1,15 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Listratsenka Stas
- * Date: 02.04.2018
- * Time: 20:17
- */
 
-namespace App\Form;
+namespace App\Form\RecoveryPassword;
 
 
-use App\Entity\User;
+use App\Entity\RecoveryPassword\CreateEmailRequest;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -26,20 +20,20 @@ class RecoveryType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class, array('label' => 'Введите email'))
-            ->add('code', TextType::class, array('label' => 'Код'))
-            ->add('plainPassword', RepeatedType::class, array(
-                'type' => PasswordType::class,
-                'first_options'  => array('label' => 'Новый пароль'),
-                'second_options' => array('label' => 'Повторите пароль'),
-            ))
-            ->add('save', SubmitType::class, array('label' => 'Войти'))
+//            ->add('code', TextType::class, array('label' => 'Код'))
+//            ->add('plainPassword', RepeatedType::class, array(
+//                'type' => PasswordType::class,
+//                'first_options'  => array('label' => 'Новый пароль'),
+//                'second_options' => array('label' => 'Повторите пароль'),
+//            ))
+            ->add('save', SubmitType::class, array('label' => 'Подтвердить'))
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => CreateEmailRequest::class,
         ]);
     }
 }
