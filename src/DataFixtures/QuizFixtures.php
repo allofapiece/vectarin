7b3ffa4cd7025a209ae0efdc0dc41b3686e15081
allofapiece <stasana1998@tut.by>
2018-04-07 01:13:34 +0300

    Added quizzes data fixtures

---
+++ src/DataFixtures/QuizFixtures.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Listratsenka Stas
+ * Date: 07.04.2018
+ * Time: 1:03
+ */
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Quiz;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class QuizFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // create 20 products! Bam!
+        for ($i = 0; $i < 20; $i++) {
+            $quiz = new Quiz();
+            $quiz->setName('Викторина № '.$i);
+            $quiz->setDescription('Очеень длинное описание для викторины №'.$i);
+            $quiz->setList('Dj');
+
+            $manager->persist($quiz);
+        }
+
+        $manager->flush();
+    }
+}
